fc2900a9d47d5205ec30d528e2439bd62f04b353
Toritseju Eyekosi <tjeyeks@gmail.com>
2022-10-05 13:49:49 +0100

    tokens

--- middleware/userAuthentication.js
+++ middleware/userAuthentication.js
@@ -14,7 +14,7 @@ export const verifyToken = async (req, res, next) => {
       try {
         token = req.headers.authorization.split(" ")[1];
         //console.log(token)
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const decoded = jwt.verify(token, "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY2MzE1MDE3NCwiaWF0IjoxNjYzMTUwMTc0fQ.dFimEhoA_itoNQnQEmX3Idh-_52kdsmILTp2pQhwg3k");
         req.user = await User.findById(decoded.id).select("-password");
         next();
       } catch (error) {
